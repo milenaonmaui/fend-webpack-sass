b21c70c049c402258b44e55fab901489f3ac10df
Milena Kari <milenakari@yahoo.com>
2021-01-25 10:55:19 -1000

    Added scss loaders

--- webpack.dev.js
+++ webpack.dev.js
@@ -11,11 +11,15 @@ module.exports = {
     module: {
         rules: [
             {
-                test: '/\.js$/',
+                test: /\.js$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
-            }
-        ]
+            },
+        {
+            test: /\.scss$/,
+            use: ['style-loader', 'css-loader', 'sass-loader']
+
+        }]
     },
     plugins: [
         new HtmlWebPackPlugin({
